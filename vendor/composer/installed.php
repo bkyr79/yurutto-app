08e868e707258350bbc1596aaf62deecc5aff76e
takaomiyamashita <ty.8479@gmail.com>
2021-02-26 10:51:17 +0900

    デプロイ準備
    composerアップデートなど

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'fe2ee9babe00c01c4f17fa76265fbd35207e4362',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fe2ee9babe00c01c4f17fa76265fbd35207e4362',
+    ),
+  ),
+);
